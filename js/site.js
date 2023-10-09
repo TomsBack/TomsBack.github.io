be132378dde02716de9003fbb3d08a290ec16c5a
TomsBack <oplpet95@gmail.com>
2023-10-09 13:29:12 +0200

    mode switching button

--- js/site.js
+++ js/site.js
@@ -1,1 +1,33 @@
-const navbarElement = document.querySelector("#navbar")
+'use strict';
+let darkMode = Boolean(localStorage.getItem("darkMode"));
+
+const themeSwitcher = document.querySelector(".theme-switcher");
+const buttonSwitcher = document.querySelector(".button-switcher");
+
+const darkIcon = document.querySelector("#dark");
+const lightIcon = document.querySelector("#light");
+
+setTheme();
+
+function setTheme() {
+    if (darkMode) {
+        buttonSwitcher.classList.remove("light-active");
+        
+        darkIcon.classList.add("active");
+        lightIcon.classList.remove("active");
+    } else {
+        buttonSwitcher.classList.add("light-active");
+
+        darkIcon.classList.remove("active");
+        lightIcon.classList.add("active");
+    }
+}
+
+buttonSwitcher.addEventListener("click", event => {
+    darkMode = !darkMode;
+
+    setTheme();
+    console.log(darkMode);
+
+    localStorage.setItem("darkMode", darkMode);
+})
